8a0203e082d08197ca27ec6e414cbff7b1f28058
Grayson Savage <graysonsavage@yahoo.com>
2015-03-31 23:07:28 -0400

    Fixed formulas, changed A_exit to area ratio...
    got rid of unneeded graphs, added comments, etc.

--- EAS 4300 HW 6.py
+++ EAS 4300 HW 6.py
@@ -61,21 +61,27 @@ def calcThrust(m_amb, F, u_in, u_out):
 def calcTo_max (T_amb,h_c,F,Cp):
     To_max = (h_c*F - Cp*0.001*T_amb)/(Cp*0.001*F + Cp*0.001)
     return To_max
-def propeffic(u_in,u_out):
+def propeffic(u_in, u_out, F, h_c):
     np = (2*(u_in/u_out))/(1+(u_in/u_out))
+#    np = (I*u_in)/((1+F)*((u_out**2)/2)-((u_in**2)/2))
     return np
-def thermeffic(u_in, u_out, To_amb, To_max, Cp):
-    nth = (.5*((u_out**2)-(u_in**2)))/(Cp*(To_max-To_amb))
+def thermeffic(u_in, u_out, To_max, T_amb):
+    nth = ((u_out**2)-(u_in**2))/(2*Cp*(To_max-To_amb))
+#    nth = ((1+F)*(u_out**2)-(u_in**2))/(2*F*h_c)
     return nth
+def arearatio(M): #I used Gamma = 1.33 just to simplify trying to type this equation
+    A_ratio = (1/M)*((2/2.33)*(1+(0.33/2)*M**2))**(2.33/0.66)
+    return A_ratio
 
+# Given values that won't change in the main loop
 R = 287
-A_exit = 1 # m^2
 P_amb = 26.500
 T_amb = 223
 P_exit = P_amb
 h_c = 43000
 Fst = 0.06
 
+# Initialize the empty lists that will hold our graph data
 Mlist = []
 Ilist = []
 TSFClist = []
@@ -88,21 +94,20 @@ Cplist = []
 Tamblist = []
 Toamblist = []
 Texitlist = []
+A_ratiolist = [] 
 
 
 # Get the range of Mach numbers to iterate the program through
 start = float(raw_input('Please enter starting Mach Number: '))
 end = float(raw_input('Please enter ending Mach Number: '))
-# Calculate needed values based on given values
+# Calculate needed values based on given values, with 0.01 step in M to smooth graphs
 for M in np.arange(start,end,0.01):
+    # Given values that need to be reset each iteration
     To_max = 2600
     Gamma = 1.40
     To_amb = TofromT(T_amb, Gamma, M)
-#    mach = str(M)
-#    print ('Mach Number = ' + mach)
     u_inlet = Velocity(M, Gamma, T_amb, R)
-#    inletVel = str(u_inlet)
-#    print('Inlet Velocity = ' + inletVel + ' m/s')
+    # Getting to the combustor Gamma changes to 1.33
     Gamma = 1.33
     Cp = calcCp(Gamma, R)
     F = FfromCp(To_amb, To_max, h_c, Cp, Fst)
@@ -111,15 +116,18 @@ for M in np.arange(start,end,0.01):
     T_exit = TfromTo(To_max, Gamma,M)
     density_exit = rho(P_exit,R,T_exit)
     u_exit = Velocity(M, Gamma, T_exit, R)
-    m_exit = m_flow(density_exit,u_exit,A_exit)
+    A_ratio = arearatio(M)
+    m_exit = m_flow(density_exit,u_exit,A_ratio)
     m_amb = m_spec(m_exit,F)
     thrust = calcThrust(m_amb, F, u_inlet, u_exit)
     I = thrust/m_amb
     TSFC = (F*m_amb)/thrust
-    np = propeffic(u_inlet, u_exit)
-    nth = thermeffic(u_inlet, u_exit, To_amb, To_max, Cp)
+    np = propeffic(u_inlet, u_exit, F, h_c)
+    nth = thermeffic(u_inlet, u_exit, To_max, To_amb)
     no = np*nth
-
+    
+    # Write our calculations to the end of the list so we can graph them after the loop
+    A_ratiolist.append(A_ratio)
     Mlist.append(M)
     Ilist.append(I)
     TSFClist.append(TSFC)
@@ -151,18 +159,11 @@ plt.title('TSFC vs Mach Number')
 
 plt.figure(2)
 plt.figure(2).subplots_adjust(hspace=0.35)
-plt.subplot(211)
-plt.plot(Mlist, Flist)
+plt.subplot(111)
+plt.plot(Mlist, To_maxlist)
 plt.xlabel('Mach Number, M')
-plt.ylabel('F')
-plt.title('F vs Mach Number')
-
-plt.figure(2)
-plt.subplot(212)
-plt.plot(Mlist, Cplist)
-plt.xlabel('Mach Number, M')
-plt.ylabel('Cp')
-plt.title('Cp vs Mach Number')
+plt.ylabel('To4')
+plt.title('To4 vs Mach Number')
 
 plt.figure(3)
 plt.figure(3).subplots_adjust(hspace=0.6)
@@ -187,33 +188,10 @@ plt.ylabel('no')
 plt.title('no vs Mach Number')
 
 plt.figure(4)
-plt.figure(4).subplots_adjust(hspace=0.35)
-plt.figure(4).subplots_adjust(wspace=0.30)
-plt.subplot(221)
-plt.plot(Mlist, To_maxlist)
+plt.subplot(111)
+plt.plot(Mlist, A_ratiolist)
 plt.xlabel('Mach Number, M')
-plt.ylabel('To_max')
-plt.title('To_max vs Mach Number')
-
-plt.figure(4)
-plt.subplot(222)
-plt.plot(Mlist, Tamblist)
-plt.xlabel('Mach Number, M')
-plt.ylabel('T_ambient')
-plt.title('T_ambient vs Mach Number')
-
-plt.figure(4)
-plt.subplot(223)
-plt.plot(Mlist, Toamblist)
-plt.xlabel('Mach Number, M')
-plt.ylabel('To_ambient')
-plt.title('To_ambient vs Mach Number')
-
-plt.figure(4)
-plt.subplot(224)
-plt.plot(Mlist, Texitlist)
-plt.xlabel('Mach Number, M')
-plt.ylabel('T_exit')
-plt.title('T_exit vs Mach Number')
+plt.ylabel('A/A*')
+plt.title('A/A* vs Mach Number')
 
 plt.show()
