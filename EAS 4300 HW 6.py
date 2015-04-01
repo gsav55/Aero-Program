fab4ec01abba65f930256d7cdf903d2eda1385dc
gsav55 <gsavage@gsavage.bg.brasfieldgorrie.com>
2015-04-01 10:57:57 -0400

    Added Pictures of Graphs for M=1 to M=6

--- EAS 4300 HW 6.py
+++ EAS 4300 HW 6.py
@@ -1,30 +1,4 @@
 # -*- coding: utf-8 -*-
-# This code is for EAS 4300 Homework 6 Problem 3 and was written by Grayson Savage on 3/30/2015
-
-# Consider an ideal ramjet engine flying at an altitude of 10,000 m (see
-# local atmospheric conditions in appendix III of the textbook). The jet fuel used
-# has a heat of combustion of 43,000 kJ/kg, and a stoichiometric fuel to air ratio of
-# 0.06. The maximum temperature of the ramjet is 2600 K. Assume the specific
-# heat ratio up to the combustion chamber is 1.4, and that the specific heat ratio
-# through the rest of the engine (including the combustor) is 1.33. The specific heat
-# at constant pressure for the gases in the combustor can be calculated using the
-# following equation
-
-# Calculate the specific thrust and TSFC as a function of flight Mach number from a range of 1 to
-# 6. Note that the fuel to air ratio can not be above the stoichiometric value (i.e. we
-# can’t burn more fuel than that at an equivalence ratio of unity). Some operating
-# points may require a higher value of f than the stoichiometric value to reach the
-# temperature limit. Under this condition, f should be set to the stoichiometric
-# value, and the combustor exit temperature T04 should be calculated from the
-# energy equation. Otherwise, the maximum temperature is used and f is calculated
-# from the energy equation. Using a procedure and an if/then/else approach in EES
-# might be ideal for this. Include the following plots:
-# 1. Specific thrust (I) vs. Mflight
-# 2. TSFC vs. Mflight
-# 3. T04 vs. Mflight
-# 4. Aexit/Athroat vs. Mflight
-# 5. ηth, ηp, and ηo vs. Mflight
-
 import math
 import matplotlib.pyplot as plt
 import numpy as np
